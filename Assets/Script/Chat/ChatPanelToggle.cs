2a67daee73b2cd9b46fe3bfb5fb434bf4fa6c795
tany0305 <tany5064@gmail.com>
2025-04-15 23:12:37 +0700

    sav

---
+++ Assets/Script/Chat/ChatPanelToggle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ChatPanelToggle : MonoBehaviour
+{
+    public GameObject chatPanel;  // Panel chứa UI chat
+    public UnityEngine.UI.Button toggleButton; // Nút để bật/tắt giao diện chat
+
+    void Start()
+    {
+        if (toggleButton != null)
+        {
+            toggleButton.onClick.AddListener(ToggleChatPanel);
+        }
+        else
+        {
+            Debug.LogError("Toggle button is not assigned!");
+        }
+    }
+
+    // Hàm để bật/tắt chat panel
+    public void ToggleChatPanel()
+    {
+        if (chatPanel != null)
+        {
+            chatPanel.SetActive(!chatPanel.activeSelf);  // Đảo ngược trạng thái hiện tại của panel
+        }
+        else
+        {
+            Debug.LogError("Chat panel is not assigned!");
+        }
+    }
+}
--- Assets/Script/Chat/ChatSystem.cs
+++ Assets/Script/Chat/ChatSystem.cs
@@ -1,4 +1,6 @@
 ﻿using Fusion;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,20 +10,35 @@ public class ChatSystem : NetworkBehaviour
     public TextMeshProUGUI textMassage;
     public TMP_InputField inputField;
     public GameObject buttonSend;
+    public GameObject chatPanel;  // Thêm tham chiếu tới panel chat
 
     //chạy game sau khi nhân vật đc spawn
     public override void Spawned()
     {
+
         textMassage = GameObject.Find("Text Message").GetComponent<TextMeshProUGUI>();
         inputField = GameObject.Find("InputField").GetComponentInParent<TMP_InputField>();
         buttonSend = GameObject.Find("Button Send");
+        chatPanel = GameObject.Find("Panel chat");  // Lấy reference tới Chat Panel
+
+        /*// Ẩn chat panel khi game bắt đầu
+        if (chatPanel != null)
+        {
+            chatPanel.SetActive(false);  // Ẩn panel chat ngay khi game bắt đầu
+        }
+        else
+        {
+            Debug.LogError("Chat panel is not assigned!");
+        }*/
+
         buttonSend.GetComponent<Button>().onClick.AddListener(SendMassageChat);
+
     }
 
     public void SendMassageChat()
     {
         var massage = inputField.text;
-        if(string.IsNullOrWhiteSpace(massage)) return;
+        if (string.IsNullOrWhiteSpace(massage)) return;
         var id = Runner.LocalPlayer.PlayerId;
         var text = $"Player {id}: {massage}";
         RpcChat(text);
@@ -34,7 +51,5 @@ public class ChatSystem : NetworkBehaviour
     public void RpcChat(string msg)
     {
         textMassage.text += msg + "\n";
-    }    
-
-    
+    }
 }
